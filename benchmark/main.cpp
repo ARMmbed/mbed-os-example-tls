01c917fd531886bb3f97b24e66bcd0e788eb6a76
Hugues Kamba <hugues.kamba@arm.com>
2019-08-15 13:20:25 +0100

    Pass an integer value for callback interval as API has been updated

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -154,7 +154,7 @@ do {                                                                        \
     mbedtls_printf(HEADER_FORMAT, TITLE);                                   \
     fflush(stdout);                                                         \
                                                                             \
-    for (i = 1, alarmed = 0, t.attach(alarm, 1.0); !alarmed; i++) {         \
+    for (i = 1, alarmed = 0, t.attach(alarm, 1); !alarmed; i++) {           \
         ret = CODE;                                                         \
         if (ret == MBEDTLS_ERR_PLATFORM_FEATURE_UNSUPPORTED) {              \
             mbedtls_printf("Feature unsupported\n");                        \
